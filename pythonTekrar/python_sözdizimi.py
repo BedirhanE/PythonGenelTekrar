5b91866dcbfa7cdd2d996a510ac81e2f2b7cfd91
Bedirhan Elçin <79866900+BedirhanE@users.noreply.github.com>
2023-04-12 23:46:34 +0300

    update

--- pythonTekrar/python_sözdizimi.py
+++ pythonTekrar/python_sözdizimi.py
@@ -1,1 +1,25 @@
-print("bedo")
+#python temel sözdizimi
+#büyük ve küçük harf
+sayı = 6
+print(sayı)
+
+#yorum satırı koymak için
+""""-büyük küçük harf 
+    -yorum
+    -girinti
+    -anahtar kelime
+ """
+#IndentationError hatası almamak için girintilere dikkat etmek zorundayız
+if 5<10:
+    print("yes")
+else:
+ print("no")
+ 
+#sayılı değişkenler
+
+sayı1=8
+sayı2=4
+
+#1sayı=1
+
+print(sayı1*sayı2)
