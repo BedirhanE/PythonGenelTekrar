1683e0c3b1d6462dcbddba00dba7ae16daf806c7
Bedirhan Elçin <79866900+BedirhanE@users.noreply.github.com>
2023-05-24 23:51:47 +0300

    update

---
+++ pythonTekrar/tuple.py
@@ -0,0 +1,21 @@
+#Tuple
+
+"""" değiştirilemez ve sıralı bir veri tipidir
+(1,2,3)
+
+"""
+
+tuple_veritipi = (1,2,3,3,4,5,6)
+#ilk elemanı bulma
+print(tuple_veritipi[0])
+
+# 2. indeksten sonraki elemanları yazdır.
+print(tuple_veritipi [2:])
+
+#tuple içerisinde 3 den kaçtane var
+
+print(tuple_veritipi.count(3))
+
+tuple_xyz = (1,2,3)
+x,y,z=tuple_xyz
+print(x,y,z)
